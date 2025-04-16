6fb1a62079c75f513db90b41c4f7d72bdb7cab7e
khirthiken <m-11395157@moe-dl.edu.my>
2025-04-16 12:31:11 +0800

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,31 @@
+import random  
+
+def main():
+    """Start a colour guessing game."""
+    print("Guess the colour!")
+
+
+rainbow = [
+    "red"
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet"
+    ]
+
+x = random.choice(rainbow)
+print(x)
+guess=None
+while x != guess:
+
+    guess = str(input("What colour am i thinking of"))
+
+    if x == guess:
+      print("You guessed {}. Congratulations you got it right!".format(guess))
+      break
+    else:
+      print("You guessed {}. Unfortunately its wrong. Try again!" .format(guess))
+
+main()
